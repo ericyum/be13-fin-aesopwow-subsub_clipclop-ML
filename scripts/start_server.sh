85b76ec9f79e0fbdf79ae8cfb7b2ee97f2efff64
YeAn475 <81356687+YeAn475@users.noreply.github.com>
2025-05-18 23:14:17 +0900

    배포테스트

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-PID=$(pgrep -f 'java -jar')
+cd /home/ubuntu/app
+
+# 1. 기존 gunicorn 프로세스 종료
+PID=$(pgrep -f 'gunicorn')
 if [ -n "$PID" ]; then
-  echo "기존 Spring Boot 프로세스 종료: $PID"
+  echo "기존 gunicorn 프로세스 종료: $PID"
   kill -9 $PID
   sleep 2
 fi
 
-echo "새로운 Spring Boot 앱 실행"
-nohup java -jar /home/ubuntu/app/*.jar --spring.profiles.active=prod > /home/ubuntu/app/app.log 2>&1 &
+# 2. 가상환경 활성화 (만약 사용한다면)
+# source venv/bin/activate
+
+# 3. 필요한 패키지 설치
+pip3 install -r requirements.txt
+
+# 4. Flask 앱 실행 (gunicorn)
+nohup gunicorn --bind 0.0.0.0:8080 app:app > app.log 2>&1 &
